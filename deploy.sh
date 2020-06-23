596f16ff2df7529ffcc87ea22d0d36438e13f482
Petr Philipp <petr.philipp@avast.com>
2020-06-23 20:59:39 +0200

    travis

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
-docker build -t petrph/multi-client:latest -t petrph/multi-worker:$SHA -f ./client/Dockerfile ./client
-docker build -t petrph/multi-server:latest -t petrph/multi-worker:$SHA -f ./server/Dockerfile ./server
+docker build -t petrph/multi-client:latest -t petrph/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t petrph/multi-server:latest -t petrph/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t petrph/multi-worker:latest -t petrph/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push petrph/multi-client
-docker push petrph/multi-server
-docker push petrph/multi-worker
+docker push petrph/multi-client:latest
+docker push petrph/multi-server:latest
+docker push petrph/multi-worker:latest
+
+docker push petrph/multi-client:$SHA
+docker push petrph/multi-server:$SHA
+docker push petrph/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment client=petrph/multi-client
-kubectl set image deployments/server-deployment server=petrph/multi-server
-kubectl set image deployments/worker-deployment worker=petrph/multi-worker
+kubectl set image deployments/client-deployment client=petrph/multi-client:$SHA
+kubectl set image deployments/server-deployment server=petrph/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=petrph/multi-worker:$SHA
